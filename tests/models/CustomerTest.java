c78714e4458fee15002ee405afbd910ceae0a386
HarmanKaur2001 <200456178@student.georgianc.on.ca>
2021-04-06 20:09:22 -0400

    add the tests here

---
+++ tests/models/CustomerTest.java
@@ -0,0 +1,56 @@
+package models;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+import static junit.framework.TestCase.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CustomerTest {
+
+    Customer customer;
+
+    @BeforeEach
+    void setUp(){
+        customer = new Customer("Harman","Kaur","39 SandWay drive",
+                "Female", LocalDate.of(2001,12,14),12345);
+    }
+
+    @Test
+    void getOrderId() {
+        assertEquals(12345, customer.getOrderId());
+    }
+
+    @Test
+    void setOrderId() {
+        assertEquals(12345, customer.getOrderId());
+    }
+
+    @Test
+    void addOrder() {
+        customer.addOrders("Food 100H");
+        ArrayList<String> expResult =  new ArrayList<>();
+        expResult.add("Food 100H");
+        assertEquals(expResult,customer.getOrders());
+    }
+
+    @Test
+    void getOrders() {
+
+    }
+
+    @Test
+    void addOrderInvalid() {
+        assertThrows(IllegalArgumentException.class, ()->
+                customer.addOrders("Deserts"));
+    }
+
+    @Test
+    void addOrderInvalid2() {
+        assertThrows(IllegalArgumentException.class, ()->
+                customer.addOrders("Cold drinks"));
+    }
+}
